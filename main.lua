ca4c5b4d237886b94da6680d407d50c1ce498f7e
Charlotte Stone <charlie.stone649@gmail.com>
2024-05-10 10:48:00 +0100

    Create main.lua and add solution to challenge

---
+++ main.lua
@@ -0,0 +1,20 @@
+local cars = {}
+
+-- returns the amount of working cars produced by the assembly line every hour
+function cars.calculate_working_cars_per_hour(production_rate, success_rate)
+  return production_rate * success_rate / 100
+end
+
+-- returns the amount of working cars produced by the assembly line every minute
+function cars.calculate_working_cars_per_minute(production_rate, success_rate)
+  return cars.calculate_working_cars_per_hour(production_rate, success_rate) // 60
+end
+
+-- returns the cost of producing the given number of cars
+function cars.calculate_cost(cars_count)
+  discount = (cars_count // 10) * 95000
+  non_discount = (cars_count % 10) * 10000 
+  return discount + non_discount
+end
+
+return cars
